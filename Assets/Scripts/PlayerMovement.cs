386ac4344a534a1102cefa603c497ba231ac1c5b
bayushi-yakusoku <bayushi.tonio@gmail.com>
2023-05-01 17:06:39 +0200

    Add screen wrap around

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
@@ -20,6 +22,21 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        TouchScreenManager();
+
+        ScreenWrapAround();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+
+        rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxVelocity);
+
+    }
+
+    private void TouchScreenManager()
     {
         if (Touchscreen.current.primaryTouch.press.isPressed)
         {
@@ -45,11 +62,19 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    private void ScreenWrapAround()
     {
-        rigidbody.AddForce(movementDirection * forceMagnitude, ForceMode.Force);
+        Vector3 newPosition = transform.position;
 
-        rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxVelocity);
+        Vector3 viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
 
+        if (viewportPosition.x < 0 || viewportPosition.x > 1)
+            newPosition.x *= -1;
+
+        if (viewportPosition.y < 0 || viewportPosition.y > 1)
+            newPosition.y *= -1;
+
+        transform.position = newPosition;
     }
+
 }
