b1081eeb9593cdc9ce6d9ff6a743ea38694b6927
bayushi-yakusoku <bayushi.tonio@gmail.com>
2023-05-01 22:20:01 +0200

    Big change! Moving the Camera on Top of the scene
    It's easier to understand the transformations of the objects

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float forceMagnitude;
     [SerializeField] private float maxVelocity;
+    [SerializeField] private float rotationSpeed;
 
     private new Rigidbody rigidbody;
     private Vector3 movementDirection;
@@ -24,6 +25,8 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         TouchScreenManager();
+
+        Rotate();
 
         ScreenWrapAround();
     }
@@ -50,7 +53,7 @@ public class PlayerMovement : MonoBehaviour
             }
 
             Vector3 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
-            worldPosition.z = transform.position.z;
+            worldPosition.y = transform.position.y;
 
             //transform.LookAt(worldPosition, Vector3.back);
             movementDirection = transform.position - worldPosition;
@@ -68,13 +71,46 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 viewportPosition = Camera.main.WorldToViewportPoint(transform.position);
 
-        if (viewportPosition.x < 0 || viewportPosition.x > 1)
-            newPosition.x *= -1;
+        if (viewportPosition.x < 0)
+            newPosition.x = -1f * (newPosition.x + 0.1f);
+        else if (viewportPosition.x > 1)
+            newPosition.x = -1f * (newPosition.x - 0.1f);
 
-        if (viewportPosition.y < 0 || viewportPosition.y > 1)
-            newPosition.y *= -1;
+        if (viewportPosition.y < 0)
+            newPosition.z = -1f * (newPosition.z + 0.1f);
+        else if (viewportPosition.y > 1)
+            newPosition.z = -1f * (newPosition.z - 0.1f);
 
         transform.position = newPosition;
     }
 
+    private void Rotate()
+    {
+        int roll = 0;
+
+        if (movementDirection == Vector3.zero) return;
+
+        Quaternion newRotation = Quaternion.Lerp(
+                transform.rotation,
+                Quaternion.LookRotation(movementDirection, Vector3.up),
+                Time.deltaTime * rotationSpeed
+            );
+
+        if (newRotation.y > transform.rotation.y)
+        {
+            Debug.Log("Roll Right");
+            roll = -1;
+        }
+        else if (newRotation.y < transform.rotation.y)
+        {
+            Debug.Log("Roll Left");
+            roll = 1;
+        }
+        else
+        {
+            roll = 0;
+        }
+
+        transform.rotation = newRotation;
+    }
 }
